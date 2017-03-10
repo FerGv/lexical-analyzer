cbc45ab054c94206f7ef80f49acb15238ccc010a
Fernando Garcia <fkngv_10@hotmail.com>
2017-03-09 20:14:45 -0600

    Explicación de expresiones regulares

--- analizador_lexico.py
+++ analizador_lexico.py
@@ -3,9 +3,11 @@ contador_lenguajes = 1 #Se inicializa el contador de lenguajes.
 bandera_inicio = False #Se inicializa la bandera que dará inicio a la búsqueda de lenguajes.
 archivo = open("LANGUAGE.TXT") #Se abre el archivo y se guarda en la variable "archivo".
 contenido = archivo.readlines() #Se leen todas la líneas del archivo y se guardan en la variable "contenido".
-patron_inicio = re.compile('[\*\s]{5,7}') #Se establece la expresion regular que indicará el inicio de la búsqueda.
-patron_lenguaje = re.compile('^[\w\*][^":,]* -') #Se establece la expresion regular que filtrará los lenguajes.
-patron_fin = re.compile('={7}') #Se establece la expresion regular que indicará el fin de la búsqueda.
+patron_inicio = re.compile('[\*\s]{5,7}') #Se establece la expresion regular que indicará el inicio de la búsqueda de lenguajes. Se buscarán de 5 a 7 asteriscos y/o espacios.
+patron_lenguaje = re.compile('^[\w\*][^":,]* -') #Se establece la expresión regular que filtrará los lenguajes.
+#^[\w\*] => Se busca al inicio de la línea que el primer caracter sea alfanumérico o asterisco.
+#[^":,]* - => Después se puede tener cualquier número de caracteres que no sean comillas dobles, dos puntos ni comas, y que después del último caracter haya un espacio y un guion.
+patron_fin = re.compile('={7}') #Se establece la expresion regular que indicará el fin de la búsqueda de lenguajes. Se buscarán 7 signos de igualdad juntos.
 for linea in contenido: #Se comienza un ciclo para recorrer línea por línea el contenido.
 	if not bandera_inicio: #Se verifica si la bandera de inicio es Falsa.
 		if patron_inicio.findall(linea): bandera_inicio = True #Si se encuentra el patron de inicio, se activa la bandera cambiando su valor a Verdadero.
